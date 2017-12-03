b55e6fb93a6c353376ec563f8f0d09c56f33cecc
Jan Fic <janfc6@gmail.com>
2017-12-03 15:58:48 -0500

    added Screens system added MainMenuScreen

--- workspace/core/src/com/greyjan/plushies/PlushiesGame.java
+++ workspace/core/src/com/greyjan/plushies/PlushiesGame.java
@@ -1,33 +1,22 @@
 package com.greyjan.plushies;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class PlushiesGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class PlushiesGame extends Game {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+    }
+    
+    
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
---
+++ workspace/core/src/com/greyjan/plushies/screens/MainMenuScreen.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.greyjan.plushies.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+/**
+ *
+ * @author Jan Fic
+ */
+public class MainMenuScreen implements Screen{
+
+    private Game game;
+
+    public MainMenuScreen(Game g) {
+        game = g;
+    }
+    
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+    
+}
