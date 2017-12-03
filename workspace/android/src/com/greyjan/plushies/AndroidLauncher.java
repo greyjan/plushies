47cbdda947364bd4557813061826fd52b078ce64
Jan Fic <janfc6@gmail.com>
2017-12-03 15:49:27 -0500

    initial commit

---
+++ workspace/android/src/com/greyjan/plushies/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.greyjan.plushies;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.greyjan.plushies.PlushiesGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PlushiesGame(), config);
+	}
+}
---
+++ workspace/desktop/src/com/greyjan/plushies/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.greyjan.plushies.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.greyjan.plushies.PlushiesGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlushiesGame(), config);
+	}
+}
